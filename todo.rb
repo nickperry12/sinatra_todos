7fa3914953ee4b02fc2c987149eae0bac2613534
Nick Perry <nick.perry12@outlook.com>
2023-09-27 18:49:22 -0700

    file updates

--- todo.rb
+++ todo.rb
@@ -2,6 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
 end
