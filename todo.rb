f2df9727e60a45fe700d049b3a9a2d965a6512f2
Nick Perry <nick.perry12@outlook.com>
2023-09-29 11:48:16 -0700

    todo project update

--- todo.rb
+++ todo.rb
@@ -1,6 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,15 +13,58 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-get "/lists" do
+# GET    /lists          --> view all lists
+# GET    /lists/new      --> new list form
+# POST   /lists          --> create new lists
+# GET    /lists/1....    --> View a single list
+
+# views all of the lists
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-get "/lists/new" do
+# render the new list form
+get '/lists/new' do
   erb :new_list, layout: :layout
+end
+
+# creates a new list, redirects back to lists page
+post '/lists' do
+  list_name = params[:list_name].strip
+  error_message = list_name_error(list_name)
+
+  if error_message
+    session[:error] = error_message
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
+
+helpers do
+  # validates list name length
+  def invalid_list_name?(list_name)
+    !(1..100).cover?(list_name.size)
+  end
+
+  # validates the list name being unique
+  def list_name_exists?(list_name)
+    session[:lists].any? { |list| list[:name] == list_name }
+  end
+
+  # either returns nil if the name is valid, or the appropriate error message
+  def list_name_error(name)
+    if invalid_list_name?(name)
+      'The list name must be between 1 and 100 characters.'
+    elsif list_name_exists?(name)
+      'The list name you have chosen already exists. Please enter a new name.'
+    end
+  end
 end
